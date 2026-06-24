329282ba042eb87deca2f6d860414b81141ffd56
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-06-24 13:17:10 +0530

    Time: 357 ms (6.58%), Space: 13.9 MB (79.23%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+
+
+// first solution 
+  vector<int> result;
+    for (int i = 0 ; i < nums.size() ; i++){
+        int count = 0 ; 
+        for (int j = 0 ; j < nums.size() ; j++){
+            if (nums[i] == nums[j]){
+                count++; 
+            }
+        }
+        if (count == 1){
+            result.push_back(nums[i]);
+        }
+    }        
+         return result ;
+    }
+};
