5b2e8ea36379d3a3237bf49c8199de63e6501fe0
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-06-24 18:14:44 +0530

    Time: 0 ms (100%), Space: 7.8 MB (53.67%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -16,15 +16,28 @@ public:
     int XOR = findXOR(s-1) ^  findXOR(e);
     XOR = XOR << 1 ;    // multiply by 2 
 
+/*  this if condition is applied if both start and n are odd integers 
+  as we know when we divide the start by 2 the lsb is removed and when n is odd ( xor of odd no. of lsb results 1  ; xor of even no of lsb results 0) so no effect occur in even lsb but gives wrong output in odd n 
+
+  example n = 5 ; start = 3 ;   
+
+   Lost bits = [1,1,1,1,1] (five ones).
+
+    XOR = 1.
+
+    Formula without fix gives 2.
+
+    Add back the 1 → 3. Correct.
+
+*/
     if (n & start & 1){  
         XOR =  XOR | 1;
     }
+
+
     return XOR ;
 
-
     }
-
-
 
     int findXOR(int n ){
     if (n % 4 == 0 )   return  n ; 
