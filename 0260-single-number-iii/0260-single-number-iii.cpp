8e1a78215a9055d3bb4c26f0d433146d3d49acca
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-06-24 13:52:35 +0530

    Time: 0 ms (100%), Space: 14 MB (55.72%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -3,19 +3,51 @@ public:
     vector<int> singleNumber(vector<int>& nums) {
 
 
-// first solution 
-  vector<int> result;
+// // first solution  tc = O(N2)   ;  sc= O(1)
+//   vector<int> result;
+//     for (int i = 0 ; i < nums.size() ; i++){
+//         int count = 0 ; 
+//         for (int j = 0 ; j < nums.size() ; j++){
+//             if (nums[i] == nums[j]){
+//                 count++; 
+//             }
+//         }
+//         if (count == 1){
+//             result.push_back(nums[i]);
+//         }
+//     }        
+//          return result ;
+
+
+
+//  BUCKET METHOD     TC = O(N)  ; SC = O(1)
+
+
+    int XOR = 0 ; 
+
+    for(int i = 0 ; i < nums.size() ; i++){
+        XOR = XOR ^ nums[i];
+    }
+
+// to find rightmost set bit (the index where both no hv diff bit)
+    // XOR = (XOR & (XOR - 1)) ^ XOR ;
+  long long mask = (long long)XOR & -(long long)XOR;
+XOR = (int)mask;
+
+
+
+    int buc1 = 0 ; // storing all the set bit at same index of XOR
+    int buc2 = 0 ; // storing all the unset bit at same index of XOR
+
     for (int i = 0 ; i < nums.size() ; i++){
-        int count = 0 ; 
-        for (int j = 0 ; j < nums.size() ; j++){
-            if (nums[i] == nums[j]){
-                count++; 
-            }
+        if (nums[i] & XOR){
+            buc1 = buc1 ^ nums[i];
+        }else {
+            buc2 = buc2 ^ nums[i];
         }
-        if (count == 1){
-            result.push_back(nums[i]);
-        }
-    }        
-         return result ;
+    }
+     
+    return {buc1 , buc2};
+
     }
 };
