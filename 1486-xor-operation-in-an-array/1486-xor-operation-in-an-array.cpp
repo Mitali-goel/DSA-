72a3f5f1dfb45c9321898380494d6bcb42caca0a
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-06-24 18:08:37 +0530

    Time: 0 ms (100%), Space: 7.9 MB (53.67%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -1,10 +1,38 @@
 class Solution {
 public:
     int xorOperation(int n, int start) {
-        int res = 0 ; 
-        for (int i =0 ; i < n ; i++){
-                res = res ^ (start + 2*i);
-        }
-        return res ;
+
+//brutte force  TC + O(N) ; SC + O(1)        
+        // int res = 0 ; 
+        // for (int i =0 ; i < n ; i++){
+        //         res = res ^ (start + 2*i);
+        // }
+        // return res ;
+
+// OPTIMIZED SOLUTION    TC O(1) ; SC + O(1)
+  
+    int s = start >> 1 ; // divide start by 2 
+    int e = s + n-1 ; 
+    int XOR = findXOR(s-1) ^  findXOR(e);
+    XOR = XOR << 1 ;    // multiply by 2 
+
+    if (n & start & 1){  
+        XOR =  XOR | 1;
     }
+    return XOR ;
+
+
+    }
+
+
+
+    int findXOR(int n ){
+    if (n % 4 == 0 )   return  n ; 
+    else if (n % 4 == 1 )   return  1 ; 
+    else if (n % 4 == 2 )   return  n+1 ; 
+    else if (n % 4 == 3 )   return  0 ;
+    else return 0 ;  
+}
+
+
 };
