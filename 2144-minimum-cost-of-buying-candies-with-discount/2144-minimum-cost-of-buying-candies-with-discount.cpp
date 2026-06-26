9aa020fc103f3b8389cca78d9d746fdfa69667ad
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-06-26 19:54:51 +0530

    Time: 0 ms (100%), Space: 14.2 MB (78.92%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     int minimumCost(vector<int>& cost) {
+
+        // tc = O(nlogn) + O(n)
+        // sc = O(1)
         int min_cost = 0 ; 
+        // sort in decending order
         sort(cost.begin(), cost.end(), greater<int>());
+        // add 0, 1, leave 2nd indx el .....
         for (int i = 0 ; i< cost.size(); i++){
             if ((i+1) % 3 != 0){
                 min_cost += cost[i];
