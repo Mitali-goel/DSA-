603ee38f77960a19e573a36a120767d8502cf9ae
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-06-26 13:28:43 +0530

    Time: 0 ms (100%), Space: 8.1 MB (42.73%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     double angleClock(int hour, int minutes) {
 
-        // just rember hr hand moves 30 deg every hr 
-        // min habd moves 6 deg every min 
+        // just remember hr hand moves 30 deg every hr 
+        // min hand moves 6 deg every min 
         // and hr hand moves 0.5 deg every min 
         hour = hour % 12;
         double hrAngle =  hour * 30 + minutes * 0.5;
