fc485de58f4f365432c138d1fdbc87b99fe3ad41
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-06-27 14:53:45 +0530

    Time: 0 ms (100%), Space: 69.5 MB (59.02%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -47,45 +47,69 @@ public:
 
 
 // optimal Solution 
-// TC = O(N)   SC = O(1)
+// TC = O(N) + O(N)         SC = O(1)
 
 
 
         int r = 0 ; int l = 0 ; int m = 0 ; 
         int max_one = 0 ; 
 
-//   this double commented part can also be removed but for mr it makes solution clear 
+//  this double commented part can also be removed but for mr it makes solution clear 
 
 
-        // while( r < nums.size() && (m < k  ||  nums[r] == 1)) {
+//         // while( r < nums.size() && (m < k  ||  nums[r] == 1)) {
+//         //     if (nums[r] == 0){
+//         //         m++;
+//         //     }
+//         //     max_one = max(max_one , r-l+1);
+//         //     r++;
+//         // }
+
+
+
+
+        // while(r < nums.size()){
         //     if (nums[r] == 0){
+        //         while (m==k){
+        //             if (nums[l] ==0){
+        //             m--;
+        //             }
+        //             l++;
+        //         }
         //         m++;
         //     }
+            
         //     max_one = max(max_one , r-l+1);
         //     r++;
         // }
+        // return max_one ; 
 
 
 
 
-        while(r < nums.size()){
-            if (nums[r] == 0){
-                while (m==k){
-                    if (nums[l] ==0){
-                    m--;
-                    }
-                    l++;
-                }
-                m++;
+
+
+
+// another optimized sol
+// TC = O(N)    SC = O(n)
+
+        while (r < nums.size() ){
+
+        if (nums[r] == 0)   m++ ;
+        
+        if (m > k) {
+            if (nums[l] == 0) {
+                m--;
             }
-            
-            max_one = max(max_one , r-l+1);
-            r++;
+            l++;
         }
-        return max_one ; 
+        if (m <= k) {
+            max_one = max(max_one , r-l+1);
+        }
+        r++;
 
-
-
+        }
+         return max_one ; 
 
 
 
