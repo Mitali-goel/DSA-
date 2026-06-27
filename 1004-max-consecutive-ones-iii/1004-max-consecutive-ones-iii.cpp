e74b282d02181b2598069e4d84b7dcaa28648edc
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-06-27 12:11:42 +0530

    Time: 0 ms (100%), Space: 69.5 MB (59.02%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -4,13 +4,13 @@ public:
 
         int r = 0 ; int l = 0 ; int m = 0 ; 
         int max_one = 0 ; 
-        // while( r < nums.size() && (m < k  ||  nums[r] == 1)) {
-        //     if (nums[r] == 0){
-        //         m++;
-        //     }
-        //     max_one = max(max_one , r-l+1);
-        //     r++;
-        // }
+        while( r < nums.size() && (m < k  ||  nums[r] == 1)) {
+            if (nums[r] == 0){
+                m++;
+            }
+            max_one = max(max_one , r-l+1);
+            r++;
+        }
 
         while(r < nums.size()){
             if (nums[r] == 0){
