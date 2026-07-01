57e306b75f57a2d405a0b5619261c2029f8aece7
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-01 22:09:43 +0530

    Time: 3 ms (45.19%), Space: 10.7 MB (18.79%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        double rev = 0 ;
-        int ori = x ;
-        int lastno = 0 ; 
-         if(x < 0) return false ;
-         while (x>0){
-            lastno = x%10 ; 
-            x = x/10;
-            rev = rev*10 + lastno ;
-         }
-         if (ori == rev){
-            return true;
-         }else {
-             return false ;
-         }
+        // double rev = 0 ;
+        // int ori = x ;
+        // int lastno = 0 ; 
+        //  if(x < 0) return false ;
+        //  while (x>0){
+        //     lastno = x%10 ; 
+        //     x = x/10;
+        //     rev = rev*10 + lastno ;
+        //  }
+        //  if (ori == rev){
+        //     return true;
+        //  }else 
+        //      return false ;
+
+
+        if(x < 0) return false ;
+        string str = to_string(x);
+        int i = 0 ; int j = str.length()-1;
+        while (i<j){
+            if (str[i] != str[j]) return false ; 
+            i++ ; j-- ;
+        }
+
+        return true ; 
+         
     }
 };
