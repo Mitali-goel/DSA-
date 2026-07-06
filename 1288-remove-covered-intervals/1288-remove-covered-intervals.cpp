4abad957e908cd1ebf20a45051ded47004f4d2fd
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-06 12:38:10 +0530

    Time: 0 ms (100%), Space: 15.3 MB (30.6%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
 int removeCoveredIntervals(vector<vector<int>>& intervals) {
-    int count = 0 ; 
-for (int i = 0 ; i < intervals.size(); i++){        
-for (int j = 0 ; j < intervals.size() ; j++){
+//     int count = 0 ; 
+// for (int i = 0 ; i < intervals.size(); i++){        
+// for (int j = 0 ; j < intervals.size() ; j++){
 
-    if(i==j){
-       continue; 
+//     if(i==j){
+//        continue; 
+//     }
+
+//     if(intervals[i][0] >= intervals[j][0] && intervals[i][1] <= intervals[j][1]){
+//         count++ ; 
+//         break ; 
+//     }
+//             }
+
+//         }
+//         return intervals.size()-count ;
+
+
+    int count= 0 ; 
+    sort(intervals.begin() , intervals.end(), [](vector<int>&a , vector<int>&b){
+        if (a[0] == b[0])
+        return a[1] > b[1];
+        return a[0] < b[0];
+    } );
+
+
+    int rightMax = INT_MIN;
+    
+    for(int i = 0 ; i < intervals.size() ; i++){
+        if (intervals[i][1] > rightMax)
+            rightMax = intervals[i][1];
+        else 
+            count++ ; 
     }
+        return intervals.size() - count ; 
 
-    if(intervals[i][0] >= intervals[j][0] && intervals[i][1] <= intervals[j][1]){
-        count++ ; 
-        break ; 
-    }
-            }
-
-        }
-        return intervals.size()-count ;
     }
 };
