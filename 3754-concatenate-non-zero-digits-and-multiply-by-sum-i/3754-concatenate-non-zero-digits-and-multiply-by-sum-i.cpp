4d2ce4b73d288737a4c51750fe7454a01a22bdc8
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-07 08:59:37 +0530

    Time: 0 ms (100%), Space: 9.5 MB (6.15%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -17,19 +17,37 @@ public:
 
 
     long long sumAndMultiply(int n) {
-        if (n == 0)      return 0 ;
+    //     if (n == 0)      return 0 ;
 
-        int x = 0 ; long long sum = 0 ; 
-        int digit = 0 ; 
-        while (n != 0){
-            digit = n%10;
-            n = n/10;
-            if (digit != 0){
-                sum += digit ; 
-                x = x*10 + digit ; 
-            }
+    //     int x = 0 ; long long sum = 0 ; 
+    //     int digit = 0 ; 
+    //     while (n != 0){
+    //         digit = n%10;
+    //         n = n/10;
+    //         if (digit != 0){
+    //             sum += digit ; 
+    //             x = x*10 + digit ; 
+    //         }
+    //     }
+    //     long long result = sum * reverse(x);
+    //     return result ;
+
+
+
+
+    if (n == 0)      return 0 ;
+    string s = to_string(n);
+    string result = "";
+    long long sum = 0 ; 
+    for(int i = 0 ; i < s.size() ; i++){
+        if (s[i] != '0'){
+            sum += s[i] - '0';
+            result += s[i];
         }
-        long long result = sum * reverse(x);
-        return result ;
+    }
+
+    int x = stoi(result);
+
+        return x * sum ; 
     }
 };
