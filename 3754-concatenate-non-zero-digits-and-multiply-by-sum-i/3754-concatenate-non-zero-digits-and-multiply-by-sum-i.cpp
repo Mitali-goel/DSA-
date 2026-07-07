7dc2cdd5233c6eecdc3075c1fd67c0a8abd19ff7
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-07 09:08:36 +0530

    Time: 0 ms (100%), Space: 8.9 MB (74.01%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -17,37 +17,48 @@ public:
 
 
     long long sumAndMultiply(int n) {
-    //     if (n == 0)      return 0 ;
 
-    //     int x = 0 ; long long sum = 0 ; 
-    //     int digit = 0 ; 
-    //     while (n != 0){
-    //         digit = n%10;
-    //         n = n/10;
-    //         if (digit != 0){
-    //             sum += digit ; 
-    //             x = x*10 + digit ; 
-    //         }
+// USING STRING CONVERSION     (let d be the digits in n )
+    //TC =  O(3d)   (d -> to_string()  , d-> loop  , d-> stoi)   
+    //SC = O(N)
+
+    // if (n == 0)      return 0 ;
+
+
+    // string s = to_string(n);
+    // string result = "";
+    // long long sum = 0 ; 
+    // for(int i = 0 ; i < s.size() ; i++){
+    //     if (s[i] != '0'){
+    //         sum += s[i] - '0';
+    //         result += s[i];
     //     }
-    //     long long result = sum * reverse(x);
-    //     return result ;
+    // }
+
+    // int x = stoi(result);
+
+    // return x * sum ; 
 
 
 
 
-    if (n == 0)      return 0 ;
-    string s = to_string(n);
-    string result = "";
-    long long sum = 0 ; 
-    for(int i = 0 ; i < s.size() ; i++){
-        if (s[i] != '0'){
-            sum += s[i] - '0';
-            result += s[i];
+
+
+// USING REVERSE FUNCTION       TC = (2d)(reverse and loop)     SC = O(1)   
+
+        if (n == 0)      return 0 ;
+
+        int x = 0 ; long long sum = 0 ; 
+        int digit = 0 ; 
+        while (n != 0){
+            digit = n%10;
+            n = n/10;
+            if (digit != 0){
+                sum += digit ; 
+                x = x*10 + digit ; 
+            }
         }
-    }
-
-    int x = stoi(result);
-
-        return x * sum ; 
+        long long result = sum * reverse(x);
+        return result ;
     }
 };
