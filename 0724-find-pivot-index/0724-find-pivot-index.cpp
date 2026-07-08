e469daf4120beb254f29e08ea5748ee41b68ad3b
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-08 11:08:27 +0530

    Time: 0 ms (100%), Space: 35.6 MB (79.72%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -2,19 +2,36 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n = nums.size();    
-        vector<int> prefix (n) ; 
-        vector<int> suffix (n) ;  
+        // vector<int> prefix (n) ; 
+        // vector<int> suffix (n) ;  
 
-        for(int i = 1 ; i < n ; i++){
-            prefix[i] = prefix[i-1] + nums[i-1];
-            suffix[n-i-1] = suffix[n-i] + nums[n-i];
-        } 
+        // for(int i = 1 ; i < n ; i++){
+        //     prefix[i] = prefix[i-1] + nums[i-1];
+        //     suffix[n-i-1] = suffix[n-i] + nums[n-i];
+        // } 
 
+        // for (int i = 0 ; i < n ; i++){
+        //     if (prefix[i] == suffix[i]){
+        //         return i;
+        //     }
+        // }     
+        // return -1 ;
+
+        int sum = 0 ; 
+        for(int i = 0 ; i < n ; i++){
+            sum += nums[i];
+        }
+
+        int left = 0 ; int right = 0 ;
         for (int i = 0 ; i < n ; i++){
-            if (prefix[i] == suffix[i]){
-                return i;
+            if (i!=0){ 
+            left += nums[i-1];
             }
-        }     
-        return -1 ;
+            right = sum - left - nums[i]; 
+            if (left == right){
+                return i ; 
+            }
+        }
+        return -1 ; 
     }
 };
