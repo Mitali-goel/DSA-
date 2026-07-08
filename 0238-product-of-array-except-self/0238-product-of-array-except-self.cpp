022a6a054db666b521fd650726f57ea6ab0db6aa
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-08 13:29:55 +0530

    Time: 0 ms (100%), Space: 40.1 MB (81.11%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,18 +2,31 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
          int n = nums.size();
-        vector<int> prefix (n , 1);
-        vector<int> suffix (n , 1);
+        // vector<int> prefix (n , 1);
+        // vector<int> suffix (n , 1);
        
-        for (int i = 1 ; i < n ; i++){
-            prefix[i] = prefix[i-1] * nums[i-1];
-            suffix[n-i-1] = suffix[n-i] * nums[n-i];
-        }
-        vector<int> result ; 
-        for (int i = 0 ; i < nums.size(); i++){
-            result.push_back(prefix[i] * suffix[i] );
+        // for (int i = 1 ; i < n ; i++){
+        //     prefix[i] = prefix[i-1] * nums[i-1];
+        //     suffix[n-i-1] = suffix[n-i] * nums[n-i];
+        // }
+        // vector<int> result ; 
+        // for (int i = 0 ; i < nums.size(); i++){
+        //     result.push_back(prefix[i] * suffix[i] );
+        // }
+
+        // return result ; 
+
+        vector<int> result(n , 1) ; 
+        for(int i = 1 ; i <n ; i++){
+           result[i] = result[i] * result[i-1] * nums[i-1];
         }
 
-        return result ; 
+        int suffix = 1 ; 
+        for(int i = n-2 ; i >=0 ; i--){
+            suffix *= nums[i+1];
+            result [i] *= suffix;
+        }
+
+        return result ;
     }
 };
