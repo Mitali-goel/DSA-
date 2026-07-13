4f4842dc3a22ee920073901269aa9bd9a532f4c9
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-13 13:18:44 +0530

    Time: 2 ms (72.23%), Space: 11 MB (41.64%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,49 +1,53 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        // int maxlen = 0 ; 
-        // for (int i = 0 ; i < s.size() ; i++){
-        //     int change = 0 ;
-        //     for (int j = i ; j < s.size() ; j++){
-        //         if(s[j] != s[i]){
-        //             if(change == k)
-        //             break ;
-        //             else {
-        //                 change++ ;
-        //             }
-        //         }
-        //         maxlen = max (maxlen,j-i+1);
-        //     }
-        // }
-        // return maxlen ; 
+    
+    // int l = 0 ; int r = 0 ; 
+    // map <char , int> mpp ; 
+    // int total = 0 ; int maxel = 0;
+    // int maxlen = 0 ; 
+    // while (r < s.size()){ 
+    //     mpp[s[r]] ++ ; 
+    //     total = 0; 
+    //     maxel = 0 ; 
+
+    //     for(auto it : mpp){
+    //         total += it.second;
+    //         maxel = max (maxel , it.second);
+    //     }
+    //     if (total - maxel > k){
+    //         mpp[s[l]] -- ; 
+    //         if (mpp[s[l]] == 0){
+    //             mpp.erase(s[l]);
+    //         }
+    //         l++ ; 
+    //     }   
+    //     maxlen = max(maxlen , r-l+1);
+    //     r++;
+    // }
+    // return maxlen ; 
+
 
 
 
 
     int l = 0 ; int r = 0 ; 
-    map <char , int> mpp ; 
-    int total = 0 ; int maxel = 0;
-    int maxlen = 0 ; 
-    while (r < s.size()){ 
-        mpp[s[r]] ++ ; 
-        total = 0; 
-        maxel = 0 ; 
+    int maxFreq = 0 ; int maxlen = 0 ;
+    vector<int> mpp (26, 0);
+    while (r < s.size()){
+        mpp[s[r] - 'A']++;
 
-        for(auto it : mpp){
-            total += it.second;
-            maxel = max (maxel , it.second);
+        maxFreq = max(maxFreq ,mpp[s[r] - 'A'] );
+
+        if ((r-l+1)- maxFreq > k){
+            mpp[s[l] - 'A']--;
+            l++;
         }
-        if (total - maxel > k){
-            mpp[s[l]] -- ; 
-            if (mpp[s[l]] == 0){
-                mpp.erase(s[l]);
-            }
-            l++ ; 
-        }   
         maxlen = max(maxlen , r-l+1);
         r++;
     }
-    return maxlen ; 
+    return maxlen;
+
 
 
 
