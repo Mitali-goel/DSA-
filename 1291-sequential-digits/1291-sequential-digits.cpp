d20bee67159435d5a57dd512947f7528bfff08d1
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-14 11:26:35 +0530

    Time: 0 ms (100%), Space: 8.4 MB (49.31%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
 
-        string s = "123456789";
-        vector<int>result ; 
-        for(int i = 0 ; i < s.size() ; i++){
-            for (int j = i ; j < s.size() ; j++){
-                string str = ""; 
-                for (int k = i ; k <= j ; k++){
-                    str += s[k];
-                }
-                if ((stoi(str)>= low) && (stoi(str) <= high)){
-                result.push_back(stoi(str));
-                }
-            }
-        }
-        sort(result.begin() , result.end()) ;
-        return result ;
+        // string s = "123456789";
+        // vector<int>result ; 
+        // for(int i = 0 ; i < s.size() ; i++){
+        //     for (int j = i ; j < s.size() ; j++){
+        //         string str = ""; 
+        //         for (int k = i ; k <= j ; k++){
+        //             str += s[k];
+        //         }
+        //         if ((stoi(str)>= low) && (stoi(str) <= high)){
+        //         result.push_back(stoi(str));
+        //         }
+        //     }
+        // }
+        // sort(result.begin() , result.end()) ;
+        // return result ;
 
 
 
@@ -42,5 +42,41 @@ public:
     //     }
     //     sort(result.begin() , result.end()) ;
     // return result ;
+
+
+
+
+
+
+
+    int lowD = 0 ; int l = low ;
+    int highD = 0 ; int h = high ; 
+    while (l != 0){
+        l = l/10;
+        lowD++ ; 
+    }while (h!= 0){
+        h = h/10;
+        highD++;
+    }
+    int diff =  highD-lowD;
+    string s = "123456789";
+    vector<int> result ;
+    for (int i = 0 ; i <= diff ; i++){
+        int left = 0 ; int right = lowD-1 ; 
+        while(right < s.size()){
+            string str = "" ; 
+            for (int j = left ; j <= right ; j++){
+                str += s[j];
+            }
+            if ((stoi(str)>= low) && (stoi(str) <= high)){
+                result.push_back(stoi(str));
+            }
+ 
+            left++ ;
+            right++ ;
+        }
+        lowD++;
+    }
+    return result ;
     }
 };
