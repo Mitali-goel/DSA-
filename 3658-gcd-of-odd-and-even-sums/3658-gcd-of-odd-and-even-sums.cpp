e9608886895fc8b551947f092dfaa78896c99977
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-15 12:14:12 +0530

    Time: 0 ms (100%), Space: 8.5 MB (46.67%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -4,22 +4,22 @@ public:
 
 // brutte force  TC = O(N)
 
-        int sumOdd = 0; 
-        int sumEven = 0 ; 
-        for (int i = 0 ; i <= 2*n ; i++){
-            if (i%2 == 0){
-                sumEven += i ; 
-            }else {
-                sumOdd += i ; 
-            }
-        }
-        int gcd = 1 ; 
-        for(int i = 2 ; i <= sumEven ; i++){
-            if (sumOdd % i == 0 && sumEven %i == 0){
-                gcd = i ; 
-            }  
-        }
-    return gcd ;
+    //     int sumOdd = 0; 
+    //     int sumEven = 0 ; 
+    //     for (int i = 0 ; i <= 2*n ; i++){
+    //         if (i%2 == 0){
+    //             sumEven += i ; 
+    //         }else {
+    //             sumOdd += i ; 
+    //         }
+    //     }
+    //     int gcd = 1 ; 
+    //     for(int i = 2 ; i <= sumEven ; i++){
+    //         if (sumOdd % i == 0 && sumEven %i == 0){
+    //             gcd = i ; 
+    //         }  
+    //     }
+    // return gcd ;
 
 
 // OPTIMAIZED           TC O(1)
@@ -32,6 +32,6 @@ public:
 
 // gcd of n*n and n*(n+1) is n itself 
 
-    // return n ;
+    return n ;
     }
 };
