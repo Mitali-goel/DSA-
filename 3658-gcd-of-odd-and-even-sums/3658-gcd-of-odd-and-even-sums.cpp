eb8bbe4a185269df1b0e4bbfe2caceadca9cc73c
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-15 12:13:33 +0530

    Time: 0 ms (100%), Space: 8.6 MB (46.67%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumOdd = 0; 
-        int sumEven = 0 ; 
-        for (int i = 0 ; i <= 2*n ; i++){
-            if (i%2 == 0){
-                sumEven += i ; 
-            }else {
-                sumOdd += i ; 
-            }
-        }
-        int gcd = 1 ; 
-        for(int i = 2 ; i <= sumEven ; i++){
-            if (sumOdd % i == 0 && sumEven %i == 0){
-                gcd = i ; 
-            }  
-        }
-    return gcd ;
+
+// brutte force  TC = O(N)
+
+    //     int sumOdd = 0; 
+    //     int sumEven = 0 ; 
+    //     for (int i = 0 ; i <= 2*n ; i++){
+    //         if (i%2 == 0){
+    //             sumEven += i ; 
+    //         }else {
+    //             sumOdd += i ; 
+    //         }
+    //     }
+    //     int gcd = 1 ; 
+    //     for(int i = 2 ; i <= sumEven ; i++){
+    //         if (sumOdd % i == 0 && sumEven %i == 0){
+    //             gcd = i ; 
+    //         }  
+    //     }
+    // return gcd ;
+
+
+// OPTIMAIZED           TC O(1)
+
+// sum of 2n even no = n * (n+1);
+
+// Sum of 2n no. = 2n * (2n+1)/2 ; ===== n * (2n+1)
+
+// so sum of 2n odd no is =  n* (2n+1) - n * (n+1) ==== n*n == n sq .
+
+// gcd of n*n and n*(n+1) is n itself 
+
+    return n ;
     }
 };
