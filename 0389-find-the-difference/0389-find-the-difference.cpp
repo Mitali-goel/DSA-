47b0e1e5d5b89887e3e43a8ccedaf0125499b8a5
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-16 19:22:09 +0530

    Time: 0 ms (100%), Space: 9.1 MB (78.8%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int>arr(26,0) ; 
-        for (int i = 0 ; i < t.size() ; i++){
-            arr[t[i] - 'a']++;
-        }
-        for (int i = 0 ; i < s.size(); i++){
-            arr[s[i] - 'a']--;
-        }
-        for (int i = 0 ; i < arr.size() ; i++){
-            if (arr[i]==1){
-                return i+'a';
-            }
-        }
-        return '0';
+        // vector<int>arr(26,0) ; 
+        // for (int i = 0 ; i < t.size() ; i++){
+        //     arr[t[i] - 'a']++;
+        // }
+        // for (int i = 0 ; i < s.size(); i++){
+        //     arr[s[i] - 'a']--;
+        // }
+        // for (int i = 0 ; i < arr.size() ; i++){
+        //     if (arr[i]==1){
+        //         return i+'a';
+        //     }
+        // }
+        // return '0';
+
+
+    char XOR = 0;
+    int i = 0 ;
+    for(i = 0 ; i < s.size() ; i++){
+        XOR = XOR ^ s[i] ^ t[i];
+    }
+    XOR = XOR ^ t[i];
+    return XOR;
     }
 };
