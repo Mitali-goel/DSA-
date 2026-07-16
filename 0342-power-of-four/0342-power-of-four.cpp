407c47ea2b9fee1b75e069dca840e607b53c4982
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-16 19:04:46 +0530

    Time: 0 ms (100%), Space: 7.9 MB (49.42%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if (n == INT_MIN)  return false;
+        if (n < 0 )  return false;
         if ((n & (n-1))== 0){
-            bool flag = 0 ; 
-            while (n!=0 && flag == 0){
-                if (n & 1){
-                flag = 1 ;
+            // bool flag = 0 ; 
+            // while (n!=0 && flag == 0){
+            //     if (n & 1){
+            //     flag = 1 ;
+            //     return true ;
+            //     }
+            //     n = n >> 2;
+            // }
+
+            if (n & 0x55555555){
                 return true ;
-                }
-                n = n >> 2;
             }
         }
         return false ; 
