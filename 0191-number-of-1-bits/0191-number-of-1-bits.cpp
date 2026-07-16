b5ac80cbff920df44a8faf69c307f1aba428f611
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-16 17:20:47 +0530

    Time: 0 ms (100%), Space: 8.2 MB (48.73%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
 
-    string deci2bin(int n ){
-        string s ; 
-        while (n!=0){
-            s.push_back(n%2);
-            n = n/2;
-        }
-        reverse(s.begin(), s.end());
-        return s ;
-    }
+    // string deci2bin(int n ){
+    // //     string s ; 
+    // //     while (n!=0){
+    // //         s.push_back(n%2);
+    // //         n = n/2;
+    // //     }
+    // //     reverse(s.begin(), s.end());
+    // //     return s ;
+    // // }
 
 
 
     int hammingWeight(int n) {
-        int count = 0;
-        string s = deci2bin(n);
-        for(int i = 0 ; i < s.size() ; i++){
-            if (s[i] == 1)
-            count++;
+    //     int count = 0;
+    //     string s = deci2bin(n);
+    //     for(int i = 0 ; i < s.size() ; i++){
+    //         if (s[i] == 1)
+    //         count++;
+    //     }
+    //     return count ; 
+
+
+
+
+
+    int count = 0 ; 
+    while (n!=0){
+        if (n%2 != 0){
+            count++ ;  
         }
-        return count ; 
+        n = n>>1 ;
+    }
+    return count ;
     }
 };
