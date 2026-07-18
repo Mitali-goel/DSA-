665105691341e168559ed7fd50728392656354ae
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-18 18:09:42 +0530

    Time: 47 ms (46.99%), Space: 134.1 MB (24.1%) - LeetHub

--- 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
+
+// zBRUTTE FORCE APPROACH             TC = O(N2) (TLE)        
         // vector<int> result;
         // vector<int>gcdPairs ;
         // for (int i = 0 ; i < nums.size(); i++){
@@ -18,7 +20,20 @@ public:
         // return result ;
 
 
+//  OPTIMAL APPROACH 
 
+// INTUTION 
+
+// firstly find maximum element in nums                                                                    TC = O(n)               SC = O(1)
+// then form a frequency vector (m is max el)                                                              TC = O(m)+O(n)          SC = O(m)
+// then form a count vector which stores count of nos divisible by i is count[i]                           TC = O(mlogm)           SC = O(m)
+// the form combinations (pair of two nos.)                                                                TC = O(m)               SC = O(1) 
+// then form a vector exact storing exact no of pairs having gdc as i is exact[i]                          TC = O(mlogn)           SC = O(m) 
+// form a prefix vector                                                                                    TC = O(m)               SC = O(m)
+// then applying lower bound of binary search
+//  (as queries arr is a 0 based index arry and prefix id 1 based) => so searching for queries[i] +1       TC = O(qlogm)            SC = O(q)
+
+// Time complexity = O(N) + O(m logm )+ O(qlogm)                   Space complexity = O(4m + q)
 
         vector<int>result ;
 
