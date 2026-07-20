011b77a943cfd408db1fbc77dc6d900191207ff4
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-20 12:56:32 +0530

    Time: 0 ms (100%), Space: 18.3 MB (39.39%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -2,23 +2,38 @@ class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
 
-        k = k % (grid.size() * grid[0].size());
-       vector<vector<int>> gridk = grid ;
-        while (k != 0){
-           grid = gridk ;
-            for (int i = 0 ; i < grid.size() ; i++){
-                for(int j = 0 ; j < grid[i].size() ; j++){
-                    if (j ==  grid[i].size()-1 && i != grid.size()-1){
-                        gridk[i+1][0] = grid[i][j];
-                    }else if (j!=  grid[i].size()-1){
-                        gridk[i][j+1] = grid[i][j];
-                    }else{
-                        gridk[0][0]= grid[i][j];
-                    }
-                }
-            }
-            k--;
+    //     k = k % (grid.size() * grid[0].size());
+    //    vector<vector<int>> gridk = grid ;
+    //     while (k != 0){
+    //        grid = gridk ;
+    //         for (int i = 0 ; i < grid.size() ; i++){
+    //             for(int j = 0 ; j < grid[i].size() ; j++){
+    //                 if (j ==  grid[i].size()-1 && i != grid.size()-1){
+    //                     gridk[i+1][0] = grid[i][j];
+    //                 }else if (j!=  grid[i].size()-1){
+    //                     gridk[i][j+1] = grid[i][j];
+    //                 }else{
+    //                     gridk[0][0]= grid[i][j];
+    //                 }
+    //             }
+    //         }
+    //         k--;
+    //     }
+    //     return gridk;
+
+
+    int index = 0 ; 
+    int m = grid.size();
+    int n = grid[0].size();
+    k = k % (m*n);
+    vector<vector<int>> result = grid ;
+
+    for(int i = 0 ; i < grid.size() ; i++){
+        for(int j = 0 ; j < grid[i].size() ; j++){
+            index = ((i*n)+j+k) % (m*n) ;
+            result[index/n][index%n] = grid[i][j];
         }
-        return gridk;
+    }
+    return result ;
     }
 };
