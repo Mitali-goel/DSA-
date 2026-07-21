42a2a55b52db9242d6281886fc2b38c0dadc701e
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-21 14:45:40 +0530

    Time: 3 ms (95.4%), Space: 8.8 MB (71.65%) - LeetHub

---
+++ 3881-direction-assignments-with-exactly-k-visible-people/3881-direction-assignments-with-exactly-k-visible-people.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long long m = 1000000007;
+    long long power (long long a , long long b ){
+        if (b == 0){
+            return 1 ;
+        }
+        long long ans = power(a , b/2) % m ;
+        long long result = ans * ans % m ;
+        if (b % 2 == 1){
+            result = result * a % m ;
+        }
+        return result ;
+    }
+
+    int countVisiblePeople(int n, int pos, int k) {
+
+        if (k > n/2){
+            k = n-1- k ;
+        }
+
+        long long num = 1 ; 
+        long long den = 1 ;
+        for (int i = 1 ; i <= k ; i++){
+            num = num * (n-i) % m;
+            den = den * i  % m;
+        }
+
+        long long reverseden = power(den , m-2) % m ;
+
+        int result = 2 *( num  % m ) * reverseden % m ;
+        return result ;
+        
+    }
+};
