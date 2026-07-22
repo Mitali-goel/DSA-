2f4eca9329d92aee2edfc865dfb52b7fb3722fde
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-22 18:26:16 +0530

    Time: 7 ms (63.84%), Space: 10.9 MB (93.79%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int n = s.size();
+        // int n = s.size();
+        // int count = 0 ; 
+        // vector<int> mpp (3,0);
+        // int l = 0 ; int r = 0 ;
+        // while(r<s.size()){
+        //     mpp[s[r] - 'a'] ++;
+        //     while(mpp[0] > 0 && mpp[1]  > 0 && mpp[2]  > 0){
+        //         count += (n - r);
+        //         mpp[s[l] - 'a']--;
+        //         l++;
+        //     }
+        //     r++;
+        // }
+        // return count ;
+
+
+
+
+
         int count = 0 ; 
-        vector<int> mpp (3,0);
-        int l = 0 ; int r = 0 ;
-        while(r<s.size()){
-            mpp[s[r] - 'a'] ++;
-            while(mpp[0] > 0 && mpp[1]  > 0 && mpp[2]  > 0){
-                count += (n - r);
-                mpp[s[l] - 'a']--;
-                l++;
+        vector<int> arr (3, -1);
+        int l = 0 ; int r = 0 ; 
+        while(r < s.size()){
+            arr[s[r] - 'a'] = r ; 
+            if (arr[0] + arr[1] + arr[2] > 0){
+                int mini1 = min (arr[0] , arr[1]);
+                int mini = min (mini1 , arr[2]);
+                count += mini +1 ;
             }
             r++;
         }
-        return count ;
+        return count;
     }
 };
