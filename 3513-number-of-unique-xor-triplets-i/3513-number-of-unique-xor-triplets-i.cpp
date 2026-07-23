f6608e410a19e3bec2cea2922315ccb44f44103d
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-23 18:14:32 +0530

    Time: 0 ms (100%), Space: 198.8 MB (60.63%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -20,61 +20,41 @@ public:
 
 
 
-
-
-
     int uniqueXorTriplets(vector<int>& nums) {
 
 // BRUTTE FORCE           TC = O(n3)(time limit exceeeds)       
 
-    //     map<int , int> mpp ;  
-    //     for(int i = 0 ; i < nums.size() ; i++){
-    //         mpp[nums[i]]++;
-    //     }
-        
-    //     int count = nums.size() ; 
-    //     for (int i = 0 ; i < nums.size() ; i++){
-    //         for (int j = i+1 ; j < nums.size() ; j++){
-    //             for (int k = j+1 ; k < nums.size() ; k++){
-    //                 int XOR = nums[i] ^ nums[j] ^ nums[k];
-    //                 if (mpp[XOR]==0) count ++;
-    //                 mpp[XOR] ++;
+//         map<int , int> mpp ;  
+//         int count = 0; 
+//         for (int i = 0 ; i < nums.size() ; i++){
+//             for (int j = i ; j < nums.size() ; j++){
+//                 for (int k = j; k < nums.size() ; k++){
+//                     int XOR = nums[i] ^ nums[j] ^ nums[k];
+//                     if (mpp[XOR]==0) count ++;
+//                     mpp[XOR] ++;
 
-    //             }
-    //         }
-    //     }
-    // return count;
+//                 }
+//             }
+//         }
+//     return count;
 
+
+
+
+// optimal solution          TC = O(long n)
 
     int n = nums.size();
 
     if (n <= 2)  return nums.size();
 
-    int count = 0 ;
+    int count = 0 ; int p = 1;
     while (n != 0){
         n = n >> 1;
         count ++;
+        p = p <<1;
     }
 
-    int p = power (2 , count);
+    // int p = power (2 , count);
     return p ;
-
-
-
-
-
-
-        // int n = nums.size();
-        // int result = n;
-        // if (n > 2) {
-        //     int comb = 1  ; 
-        //     for (int i = n ; i > n-3 ; i--){
-        //         comb *= i ;  
-        //     }
-        //     comb /= 6 ;
-        //     result += comb ;
-        // }
-
-        // return result ; 
     }
 };
