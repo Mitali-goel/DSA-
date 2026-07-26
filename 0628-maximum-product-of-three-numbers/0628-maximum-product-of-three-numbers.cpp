b821be261ca9bcbed46aca5ea272ee6e7f027f0f
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-26 23:03:02 +0530

    Time: 0 ms (100%), Space: 31.5 MB (76.45%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -4,13 +4,11 @@ public:
         sort(nums.begin() , nums.end());
         int n = nums.size();
         int first = nums[n-1] ;
-        nums.pop_back();
-        n = n-1 ;
         int product = 1 ; 
-        if ((first * nums[0] * nums[1]) > (first * nums[n-1] * nums[n-2])){
+        if ((first * nums[0] * nums[1]) > (first * nums[n-3] * nums[n-2])){
             product = nums[0] * nums[1] * first;
         }else{
-            product = nums[n-1] * nums[n-2] * first;
+            product = nums[n-3] * nums[n-2] * first;
         }
         return product  ;
     }
