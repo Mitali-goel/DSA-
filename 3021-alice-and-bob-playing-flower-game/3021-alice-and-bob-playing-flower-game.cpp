d0e4422e17d72820c0d585a7b733521e65e159d2
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-27 18:06:20 +0530

    Time: 0 ms (100%), Space: 8.4 MB (50.6%) - LeetHub

--- 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.cpp
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-        long long nodd ;
-        if (n % 2 != 0)     nodd = n /2 + 1  ; 
-        else                nodd = n /2 ; 
-        long long neven = n/2;
-        long long modd;
-        if (m % 2 != 0)     modd = m/2 +1;
-        else                modd = m/2;
-        long long meven = m/2 ;
+    //     long long nodd ;
+    //     if (n % 2 != 0)     nodd = n /2 + 1  ; 
+    //     else                nodd = n /2 ; 
+    //     long long neven = n/2;
+    //     long long modd;
+    //     if (m % 2 != 0)     modd = m/2 +1;
+    //     else                modd = m/2;
+    //     long long meven = m/2 ;
 
-    return (nodd * meven) + (neven * modd);
+    // return (nodd * meven) + (neven * modd);
+
+
+
+    return (long long) n*m /2 ;
     }
 };
