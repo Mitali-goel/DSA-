d08e341531d6c41ab4303897b7ccd6bd5009ed7a
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-30 11:40:39 +0530

    Time: 0 ms (100%), Space: 8.5 MB (31.81%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int count = 0 ; int add = 1 ; 
-        for (int i = 0 ; i < word.size() ; i++){
-            add = (i/8)+ 1 ;
-            count += add ; 
+        // int count = 0 ; int add = 1 ; 
+        // for (int i = 0 ; i < word.size() ; i++){
+        //     add = (i/8)+ 1 ;
+        //     count += add ; 
+        // }
+        // return count ; 
+
+
+        int n = word.size();
+        int count = 0 ; 
+        int i = 1  ;
+        while(n >= 0){ 
+            if (n >= 8)     count += i * 8 ;
+            else            count += i * n ;
+            n = n-8 ;
+            i++ ; 
         }
         return count ; 
     }
