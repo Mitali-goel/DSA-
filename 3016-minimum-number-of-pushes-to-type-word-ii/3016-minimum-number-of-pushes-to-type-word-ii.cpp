d6351687322e6584fe0775d6b12fbcfcb4494c3e
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-07-31 19:53:51 +0530

    Time: 4 ms (86.59%), Space: 25.4 MB (72.47%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int>count(26 , 0);
+        for (int i = 0 ; i < word.size() ; i++){
+            count[word[i] - 'a']++;
+        }
+
+        int n = 0;
+        for(int i = 0 ; i <count.size() ; i++){
+            if (count[i] != 0){
+                n++;
+            }
+        }
+
+        if (n <= 8){
+            return word.size();
+        }
+        sort(count.begin() , count.end() , greater<int>());
+
+        int cnt = 0 ; 
+        int add = 1 ; 
+        for(int i = 0 ; i < n ; i++){
+            add = i / 8 + 1 ; 
+            for(int j = 0 ; j < count[i] ; j++){
+                cnt += add ; 
+            }
+        }
+        return cnt ; 
+    }
+};
