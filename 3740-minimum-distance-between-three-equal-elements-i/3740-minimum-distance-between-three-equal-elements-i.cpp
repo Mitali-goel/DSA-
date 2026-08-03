0a3271962b5c670575d671a360557096e6a649f3
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-08-03 20:56:27 +0530

    Time: 1 ms (67.74%), Space: 28.6 MB (99.68%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -1,33 +1,53 @@
 class Solution {
 public:
     int minimumDistance(vector<int>& nums) {
-        map<int , int>mpp ; 
-        for(int i = 0 ; i < nums.size() ; i++){
-            mpp[nums[i]]++ ; 
-        }
-        int l = 0 ; int r = 0 ; int result = INT_MAX ; 
-        while (r<nums.size()){
-            if (mpp[nums[l]] >= 3){
-                int middle = l+1 ; 
-                while(nums[middle] != nums[l]){
-                    middle++ ;
+        int ans = INT_MAX ;
+        int n = nums.size(); 
+        for (int i = 0 ; i < n - 2 ; i++){
+            for (int j = i+1 ; j < n -1 ; j++){
+                if (nums[i] != nums[j]){
+                    continue ;
                 }
-                r = middle + 1 ;
-                while (nums[r] != nums[l]){
-                    r++ ;
-                }
-                result = min( result , 2*(r-l));
-                mpp[nums[l]]-- ; 
-                l++ ; 
-                r = l ;
-                middle = 0 ; 
-                
-            }else{
-                l++ ; 
-                r++ ; 
+                for(int k = j+1  ; k < n ; k++){
+                    if (nums[i] == nums[k]){
+                        ans = min(ans , 2*(k-i));
+                    }
+                } 
             }
         }
-        if (result == INT_MAX){  return -1 ; }
-      return result ;   
+        if (ans == INT_MAX) return -1 ;
+        return ans ; 
+
+
+
+
+    //     map<int , int>mpp ; 
+    //     for(int i = 0 ; i < nums.size() ; i++){
+    //         mpp[nums[i]]++ ; 
+    //     }
+    //     int l = 0 ; int r = 0 ; int result = INT_MAX ; 
+    //     while (r<nums.size()){
+    //         if (mpp[nums[l]] >= 3){
+    //             int middle = l+1 ; 
+    //             while(nums[middle] != nums[l]){
+    //                 middle++ ;
+    //             }
+    //             r = middle + 1 ;
+    //             while (nums[r] != nums[l]){
+    //                 r++ ;
+    //             }
+    //             result = min( result , 2*(r-l));
+    //             mpp[nums[l]]-- ; 
+    //             l++ ; 
+    //             r = l ;
+    //             middle = 0 ; 
+                
+    //         }else{
+    //             l++ ; 
+    //             r++ ; 
+    //         }
+    //     }
+    //     if (result == INT_MAX){  return -1 ; }
+    //   return result ;   
     }
 };
