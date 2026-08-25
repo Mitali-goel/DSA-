0246e6195eb5e9317ec3cecd918707ccb1e30dab
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-08-25 23:40:50 +0530

    Time: 0 ms (100%), Space: 24 MB (82.33%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        int i;
-        for (i = 1 ; i  <= nums.size() ; i++){
-            int j ;
-            for (j = 0 ; j < nums.size() ; j++){
-                if (nums[j] == i * k){
+        // int i;
+        // for (i = 1 ; i  <= nums.size() ; i++){
+        //     int j ;
+        //     for (j = 0 ; j < nums.size() ; j++){
+        //         if (nums[j] == i * k){
+        //             break ;
+        //         }
+        //     }
+        //     if (j == nums.size()){
+        //         return i * k ;
+        //     }
+        // }
+        // return i * k ;
+
+
+        sort(nums.begin() , nums.end());
+        for (int i = 1 ; i <= nums.size() + 1 ; i++){
+            int low = 0 ; int high = nums.size() - 1;
+            while (low <= high){
+                int mid = (low + high)/2;
+                if (nums[mid] == i * k){
                     break ;
+                }else if (nums[mid] > i * k){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
                 }
             }
-            if (j == nums.size()){
-                return i * k ;
+            if (low > high){
+                return i * k;
             }
         }
-        return i * k ;
+        return 0;
     }
 };
