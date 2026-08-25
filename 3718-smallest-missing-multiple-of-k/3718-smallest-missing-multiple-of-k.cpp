099fef664542d31c80d366d84123a37f14f78d4e
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-08-25 23:45:13 +0530

    Time: 2 ms (42.52%), Space: 25.2 MB (24.36%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
+    // linear search O(n2)
+
         // int i;
         // for (i = 1 ; i  <= nums.size() ; i++){
         //     int j ;
@@ -16,23 +18,39 @@ public:
         // return i * k ;
 
 
-        sort(nums.begin() , nums.end());
+
+// sorting and binary search O(n logn)
+
+        // sort(nums.begin() , nums.end());
+        // for (int i = 1 ; i <= nums.size() + 1 ; i++){
+        //     int low = 0 ; int high = nums.size() - 1;
+        //     while (low <= high){
+        //         int mid = (low + high)/2;
+        //         if (nums[mid] == i * k){
+        //             break ;
+        //         }else if (nums[mid] > i * k){
+        //             high = mid - 1;
+        //         }else{
+        //             low = mid + 1;
+        //         }
+        //     }
+        //     if (low > high){
+        //         return i * k;
+        //     }
+        // }
+        // return 0;
+
+// hash map O(n)       space = O(n)
+
+        map<int , int> mpp;
+        for (int i = 0 ; i < nums.size() ; i++){
+            mpp[nums[i]]++;
+        }
         for (int i = 1 ; i <= nums.size() + 1 ; i++){
-            int low = 0 ; int high = nums.size() - 1;
-            while (low <= high){
-                int mid = (low + high)/2;
-                if (nums[mid] == i * k){
-                    break ;
-                }else if (nums[mid] > i * k){
-                    high = mid - 1;
-                }else{
-                    low = mid + 1;
-                }
-            }
-            if (low > high){
-                return i * k;
+            if (mpp[i * k] == 0){
+                return i * k ;
             }
         }
-        return 0;
+        return 0 ;
     }
 };
