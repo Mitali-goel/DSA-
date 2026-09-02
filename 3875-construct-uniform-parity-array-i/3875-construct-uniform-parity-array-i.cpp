c3dd25fe9e53e083f416c995fd8907491424a31d
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-09-02 09:56:50 +0530

    Time: 0 ms (100%), Space: 30.3 MB (67.8%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
+        
+        // logic behind this is let say we have num1 all elements are even or all elements are odd then we dont have any problem always return true 
+
+        // now let say we hv both even as well as odd elements then when we subtract odd - even is always odd so we can always form a odd parity nums2 for this case
+
         return true;
+
     }
 };
