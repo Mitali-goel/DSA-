4e120cb675c25adf1703d4898ccb1031722d2d8b
Mitali-goel <mitaligoyalmitali@gmail.com>
2026-09-11 00:00:43 +0530

    Time: 0 ms (100%), Space: 8.8 MB (47.74%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isLongPressedName(string name, string typed) {
+        int i = 0 , j = 0 ;
+        while( j < typed.size()){
+            if(name[i] == typed[j]){
+                if (i < name.size()) i++;
+                j++;
+            }else if( name[i] != typed[j]){
+                if(i > 0 && name[i-1] == typed[j])
+                j++;
+                else
+                return false ;
+            }
+        }
+        if (i != name.size()){
+            return false ;
+        }
+        return true ;
+    }
+};
